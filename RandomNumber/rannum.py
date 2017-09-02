0c44b07bb524f50f894a0ab896ef10138ffacbaa
Tyler Citrin <tcitrin@umail.iu.edu>
2017-09-02 18:46:25 -0400

    Update rannum.py

--- RandomNumber/rannum.py
+++ RandomNumber/rannum.py
@@ -1,6 +1,4 @@
 from random import *
-#num = randrange(0,100)
-#print num
 
 #number guesser
 number = int(input("enter a number between 1 and 100 >>> "))
@@ -11,7 +9,6 @@ def divider(number):
 	guess = randrange(low,upp)
 	print guess
 	while (guess != number and runner==True):
-		
 		if guess == number:
 			print "your number is %r" % number
 			runner = False
@@ -22,16 +19,9 @@ def divider(number):
 				low = guess
 				guess = randrange(guess,upp)
 				print low, upp, guess
-				#return guess
-
 			elif answer == "no" or answer == "No":
 				upp = guess
 				guess = randrange(low,guess)
 				print low, upp, guess
-				#return guess
-				#print 'Your number is %r' % number
-
-		#print 'Your number is %r' % number
-
 			
 divider(number)
